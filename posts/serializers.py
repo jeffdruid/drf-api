b67c532f87c58da0f87f84854571485c2ad61d4d
JeffersonAguiar <jfdruida@gmail.com>
2024-10-12 21:15:23 +0100

    Add image_filter field to Post model

--- posts/serializers.py
+++ posts/serializers.py
@@ -8,6 +8,15 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source="owner.profile.id")
     profile_image = serializers.ReadOnlyField(source="owner.profile.image.url")
 
+    def validate_image(self, value):
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError("Image file too large ( > 2mb )")
+        if value.image.width > 4096 or value.image.height > 4096:
+            raise serializers.ValidationError(
+                "Image width or height is too large"
+            )
+        return value
+
     class Meta:
         model = Post
         fields = ["__all__"]
