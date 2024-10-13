a84e7f4edef70512f2b924dccdc8b4f930240feb
JeffersonAguiar <jfdruida@gmail.com>
2024-10-13 23:51:27 +0100

    Add followers app and update urls.py

---
+++ followers/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from django.db import IntegrityError
+from .models import Follower
+
+
+class FollowerSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source="owner.username")
+    followed_name = serializers.ReadOnlyField(source="followed.username")
+
+    class Meta:
+        model = Follower
+        fields = [
+            "id",
+            "owner",
+            "created_at",
+            "followed",
+            "followed_name",
+        ]
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError(
+                {"detail": "You are already following this user."}
+            )
