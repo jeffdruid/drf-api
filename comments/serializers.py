b53f2d433a42fa3e52e22d3959d23f913bdfa54b
JeffersonAguiar <jfdruida@gmail.com>
2024-10-14 16:14:32 +0100

    Refactor serializers to include naturaltime for created_at and updated_at fields

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Comment
 
@@ -7,6 +8,8 @@ class CommentSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source="owner.profile.id")
     profile_image = serializers.ReadOnlyField(source="owner.profile.image.url")
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
 
     class Meta:
         model = Comment
@@ -27,6 +30,12 @@ class CommentSerializer(serializers.ModelSerializer):
         if request:
             return obj.owner == request.user
         return False
+
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
 
 
 class CommentDetailSerializer(CommentSerializer):
