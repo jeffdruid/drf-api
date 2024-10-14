d3a631f4cbfb49fa86420be39455a35644413859
JeffersonAguiar <jfdruida@gmail.com>
2024-10-14 16:55:22 +0100

    Refactor ALLOWED_HOSTS

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -65,7 +65,11 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEV" in os.environ
 
-ALLOWED_HOSTS = ["localhost", "drf-api-jeff.herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = [
+    "localhost",
+    "drf-api-jeff-00b8a22f06d7.herokuapp.com",
+    "127.0.0.1",
+]
 
 
 # Application definition
