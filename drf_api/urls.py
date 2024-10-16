389f12c066a4f4e7519c60e1b56b2ac1edab2c2c
JeffersonAguiar <jfdruida@gmail.com>
2024-10-16 12:39:34 +0100

    Refactor urls.py to include TokenRefreshView for token refresh functionality

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -18,6 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import root_route, logout_route
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("", root_route),
@@ -28,6 +29,11 @@ urlpatterns = [
     path("dj-rest-auth/", include("dj_rest_auth.urls")),
     path(
         "dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")
+    ),
+    path(
+        "dj-rest-auth/token/refresh/",
+        TokenRefreshView.as_view(),
+        name="token_refresh",
     ),
     path("", include("profiles.urls")),
     path("", include("posts.urls")),
