124f7d3c132cb8fda5cda30f4895da6812ed9934
JeffersonAguiar <jfdruida@gmail.com>
2024-10-23 15:07:07 +0100

    Updated TRIGGER_WORDS List Including Slang, Code Words, and Abbreviations

--- myapi/moderation/views.py
+++ myapi/moderation/views.py
@@ -3,8 +3,25 @@ from rest_framework import status, viewsets
 from .models import FlaggedContent
 from .serializers import FlaggedContentSerializer
 
-# Trigger words list
-TRIGGER_WORDS = ["distress", "depression", "anxiety", "suicide"]
+# Trigger words list including slang, code words, and abbreviations
+TRIGGER_WORDS = [
+    # Suicide-related
+    "suicide", "suicidal", "kill myself", "kms", "k/ys", "kys", "end my life", "take my life", 
+    "hang myself", "overdose", "OD", "CO", "SW", "RIP", "x_x", "88", "CTB", "catch the bus", 
+    "deep sleep", "permanent solution", "exit plan", "final exit", "punch out", "long nap", 
+    "peaceful pill", "golden gate", "rope", "helium hood", "blackout method",
+    
+    # Self-harm-related
+    "self-harm", "SI", "SH", "C/S", "cut myself", "cutting", "slit wrists", "hurt myself", 
+    "burn myself", "scar myself", "self-injury", "bleed out", "razor", "slicing", "red bracelet", 
+    "scratching", "carving", "blade", "hurting", "hurting oneself", "pinky promise",
+
+    # Depression and Distress-related
+    "depression", "empty", "hollow", "hopeless", "worthless", "FML", "TFW", "black dog", "numb", 
+    "dead inside", "I'm done", "can't deal", "at the end", "low", "trapped", "nobody cares", 
+    "suffocate", "over it", "drowning", "zoned out", "EOD", "no way out", "mentally gone", 
+    "DGAF", "SAD"
+]
 
 def check_trigger_words(content):
     for word in TRIGGER_WORDS:
