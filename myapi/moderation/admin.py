4f60e60bb29e077a3256df1dde1a7704169622ac
JeffersonAguiar <jfdruida@gmail.com>
2024-10-25 01:12:23 +0100

    Refactor admin.py to improve FlaggedContentAdmin display and filtering

--- myapi/moderation/admin.py
+++ myapi/moderation/admin.py
@@ -1,17 +1,8 @@
 from django.contrib import admin
 from .models import FlaggedContent
 
+@admin.register(FlaggedContent)
 class FlaggedContentAdmin(admin.ModelAdmin):
-    list_display = ('user', 'post_id', 'reason', 'flagged_at', 'reviewed')
-    list_filter = ('reviewed', 'flagged_at')
+    list_display = ('user', 'post_id', 'reason', 'flagged_at', 'reviewed')  # Referencing the model fields
     search_fields = ('user__username', 'post_id', 'reason')
-
-    # Add actions to mark content as reviewed
-    actions = ['mark_as_reviewed']
-
-    def mark_as_reviewed(self, request, queryset):
-        queryset.update(reviewed=True)
-        self.message_user(request, "Selected flagged content has been marked as reviewed.")
-    mark_as_reviewed.short_description = "Mark selected content as reviewed"
-
-admin.site.register(FlaggedContent, FlaggedContentAdmin)
+    list_filter = ('reviewed', 'flagged_at')  # These fields must exist in the model
