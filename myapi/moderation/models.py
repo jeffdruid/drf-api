4c991174171e6c91a1aed94aeb7a2669c8397532
JeffersonAguiar <jfdruida@gmail.com>
2024-10-25 01:50:33 +0100

    Refactor FlaggedContent model to add is_visible field

--- myapi/moderation/models.py
+++ myapi/moderation/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 class FlaggedContent(models.Model):
     post_id = models.CharField(max_length=100)  # Firebase post ID
@@ -8,7 +7,8 @@ class FlaggedContent(models.Model):
     user = models.CharField(max_length=255)  # Firebase user UID
     flagged_at = models.DateTimeField(auto_now_add=True)
     reviewed = models.BooleanField(default=False)
-
+    is_visible = models.BooleanField(default=False)  # New field to control post visibility
 
     def __str__(self):
-        return f"Flagged by {self.user} for {self.reason}"  # Remove .username since user is a CharField
+        return f"Flagged by {self.user} for {self.reason}"
+
